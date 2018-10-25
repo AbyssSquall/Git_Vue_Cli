28b0ba6bfe28b4bc18437ef24136cd390821fdc9
MikeSquall <mikeblizzard@live.com>
2018-10-25 13:50:41 +0800

    squall的更新

---
+++ Squall_Web_Demo/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>僕はキミのこどが好きでした‼</h1>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
