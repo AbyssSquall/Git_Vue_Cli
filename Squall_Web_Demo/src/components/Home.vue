8c29558cc311079584538fa835b5e34cd123f790
MikeSquall <mikeblizzard@live.com>
2018-10-29 18:05:39 +0800

    1.0.001

--- Squall_Web_Demo/src/components/Home.vue
+++ Squall_Web_Demo/src/components/Home.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
-        <h1>僕はキミのこどが好きでした‼</h1>
+        <h1>任务列表</h1>
+        <ul>
+          <li>每个页面需要添加cookies验证或者sesions</li>
+          <li>测试微信接口</li>
+          <li>制作借车页面</li>
+          <li>获取定位信息</li>
+          <li>建立个人数据库</li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
         <foot-nav :page="page"></foot-nav>
     </div>
 </template>
