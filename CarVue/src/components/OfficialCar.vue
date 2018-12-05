e87f1108107b1e56fe2ae9abe2485a3ef21173ca
Squall <mikeblizzard@live.com>
2018-12-05 12:02:40 +0800

    test

--- CarVue/src/components/OfficialCar.vue
+++ CarVue/src/components/OfficialCar.vue
@@ -2,10 +2,10 @@
     <div>
         <h1>公务用车</h1>
         <img class="squall_wrapblank" src="static/blank.png">
-        <el-form :model="form" :rules="rules" ref="form" label-width="100px" class="demo-ruleForm">
-            <el-form-item label="使用范围" prop="range">
+        <el-form :model="squall_form" :rules="rules" ref="squall_form" label-width="100px" class="demo-ruleForm">
+            <el-form-item label="使用范围" prop="region">
                 <el-col :span="20">
-                <el-select class="squall_width_full" v-model="form.range" placeholder="请选择范围">
+                <el-select class="squall_width_full" v-model="squall_form.region" placeholder="请选择范围">
                     <el-option label="市区内" value="inScity"></el-option>
                     <el-option label="大市区内" value="inBcity"></el-option>
                     <el-option label="大市区外" value="outcity"></el-option>
@@ -14,40 +14,40 @@
             </el-form-item>
             <el-form-item label="目的地" prop="aim">
                 <el-col :span="20">
-                <el-input v-model="form.aim"></el-input>
+                <el-input v-model="squall_form.aim"></el-input>
                 </el-col>
             </el-form-item>
             <el-form-item label="开始日期" required>
                 <el-col :span="20">
                 <el-form-item prop="startdate">
-                    <el-date-picker id="startdate" type="date" placeholder="选择日期" v-model="form.startdate" style="width: 100%;"></el-date-picker>
+                    <el-date-picker id="startdate" type="date" placeholder="选择日期" v-model="squall_form.startdate" style="width: 100%;"></el-date-picker>
                 </el-form-item>
                 </el-col>
             </el-form-item>
             <el-form-item label="开始时间" required>
                 <el-col :span="20">
                 <el-form-item prop="starttime">
-                    <el-time-select :picker-options="{start: '00:00',step: '01:00',end: '23:00'}" id="starttime" placeholder="选择时间" v-model="form.starttime" style="width: 100%;"></el-time-select>
+                    <el-time-select :picker-options="{start: '00:00',step: '01:00',end: '23:00'}" id="starttime" placeholder="选择时间" v-model="squall_form.starttime" style="width: 100%;"></el-time-select>
                 </el-form-item>
                 </el-col>
             </el-form-item>
             <el-form-item label="结束日期" required>
                 <el-col :span="20">
                     <el-form-item prop="enddate">
-                        <el-date-picker id="enddate" type="date" placeholder="选择日期" v-model="form.enddate" style="width: 100%;"></el-date-picker>
+                        <el-date-picker id="enddate" type="date" placeholder="选择日期" v-model="squall_form.enddate" style="width: 100%;"></el-date-picker>
                     </el-form-item>
                 </el-col>
             </el-form-item>
             <el-form-item label="结束时间" required>
                 <el-col :span="20">
                     <el-form-item prop="endtime">
-                        <el-time-select :picker-options="{start: '00:00',step: '01:0',end: '23:00'}" id="endtime" placeholder="选择时间" v-model="form.endtime" style="width: 100%;"></el-time-select>
+                        <el-time-select :picker-options="{start: '00:00',step: '01:0',end: '23:00'}" id="endtime" placeholder="选择时间" v-model="squall_form.endtime" style="width: 100%;"></el-time-select>
                     </el-form-item>
                 </el-col>
             </el-form-item>
-            <el-form-item label="事由" prop="reason">
+            <el-form-item label="事由" prop="task">
                 <el-col :span="20">
-                <el-input style="resize:none;" type="textarea" v-model="form.reason"></el-input>
+                <el-input style="resize:none;" type="textarea" v-model="squall_form.task"></el-input>
                 </el-col>
             </el-form-item>
             <el-row>
@@ -57,7 +57,7 @@
             </el-row>
             <el-row>
                 <el-col :span="20" :offset="2">
-                    <el-button class="squall_width_full squall_top_gap" type="primary" @click="submitForm('ruleForm')">提交</el-button>
+                    <el-button class="squall_width_full squall_top_gap" type="primary" @click="submitForm('squall_form')">提交</el-button>
                 </el-col>
             </el-row>
         </el-form>
@@ -84,8 +84,8 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
       page: 'car',
-        form:{
-            range:"请选择范围",
+        squall_form:{
+            region:"请选择范围",
             driver:"",
             startdate:"2018-11-27",
             starttime:"12:01:07",
@@ -112,7 +112,7 @@ export default {
             resource: [
                 { required: true, message: '请选择活动资源', trigger: 'change' }
             ],
-            reason: [
+            task: [
                 { required: true, message: '请填写事由', trigger: 'blur' }
             ]
         },
@@ -146,12 +146,24 @@ export default {
             this.show_html = "<div class='squall_show_body'>" + squall_html + "</div>";
         },
         submitForm:function(data){
-            //console.log(this.form);
-            var  squall_html = "http://192.168.10.144:8046/table/submit";
-            this.$http.get(squall_html,{"params":this.form}).then(function(res){
-                console.log(res);
-            },function(res){
-                alert(res.status);
+            var squall_data = JSON.stringify(this[data]);
+            var squall_data_json = JSON.parse(squall_data);
+            squall_data_json.starttime = squall_data_json.startdate + " " + squall_data_json.starttime;
+            squall_data_json.endtime = squall_data_json.enddate + " " + squall_data_json.endtime;
+            squall_data_json.startdate = undefined;
+            squall_data_json.enddate = undefined;
+            squall_data_json.序号 = 178;
+            
+            var that = this;
+            this.$refs[data].validate(function(squall_bool,squall_res){
+                if(squall_bool)
+                {
+                    that.basic.squall_basic_http.PostForm(JSON.stringify(squall_data_json),"official_application");
+                }
+                else
+                { 
+                    console.log(squall_res);
+                }
             })
         }
   }
