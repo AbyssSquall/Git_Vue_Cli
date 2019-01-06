6376cf8e6d555079ba39067332d391873408a6c5
MikeSquall <mikeblizzard@live.com>
2019-01-06 16:47:28 +0800

    drawmap update

--- Squall_Web_Demo/src/components/MapDraw.vue
+++ Squall_Web_Demo/src/components/MapDraw.vue
@@ -6,15 +6,42 @@
           </el-col>
       </el-row>
       <el-row>
-        <div class="squall_Map">
-          <div id="map" style="height:100%"></div>
-        </div>
-        <ul class="squall_ToolBar">
-          <el-button>画点</el-button>
-          <el-button>画线</el-button>
-          <el-button>画面</el-button>
-          <el-button>清除</el-button>
-        </ul>
+        <el-tabs v-model="TabActive" type="card" @tab-click="handleClick">
+          <el-tab-pane label="绘制点" name="DrawPoint">
+            <el-row>
+              <div class="squall_Map_tab">
+                <div id="map1" style="height:100%"></div>
+              </div>
+              <ul class="squall_ToolBar">
+                <el-button  @click="squall_DrawPoint(this)">{{DrawPointLabel}}</el-button>
+                <el-button @click="squall_clear">清除</el-button>
+                <el-button @click="squall_show" >展示所得到的点</el-button>
+              </ul>
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="绘制线" name="DrawLine">
+            <el-row>
+              <div class="squall_Map_tab">
+                <div id="map2" style="height:100%"></div>
+              </div>
+              <ul class="squall_ToolBar">
+                <el-button>{{DrawLineLabel}}</el-button>
+                <el-button @click="squall_clear">清除</el-button>
+              </ul>
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="绘制面" name="DrawPolygon">
+            <el-row>
+              <div class="squall_Map_tab">
+                <div id="map3" style="height:100%"></div>
+              </div>
+              <ul class="squall_ToolBar">
+                <el-button>{{DrawPolygonLabel}}</el-button>
+                <el-button @click="squall_clear">清除</el-button>
+              </ul>
+            </el-row>
+          </el-tab-pane>
+        </el-tabs>
       </el-row>
     </div>
 </template>
@@ -26,31 +53,76 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      page: 'personal'
+      page: 'personal',
+      TabActive:"DrawPoint",
+      ActiveMap:{},
+      DrawPointLabel:"画点",
+      DrawLineLabel:"画线",
+      DrawPolygonLabel:"画面",
+      AimPoint:{},
+      AimPointArray:{}
     }
   },
   components: {
     //'SideNav':SideNav,
   },
   mounted:function(){
-    var map_option = {
-      "ID":"map",
+    var map_option1 = {
+      "ID":"map1",
       "Plugin":["leaflet_draw"]
       };
-    var squall_map = new Map(map_option);
+    var squall_map1 = new Map(map_option1);
+    this.ActiveMap.DrawPoint = squall_map1;
 
-    //绘制开始时触发的函数
-    function squall_start(){
+    var map_option2 = {
+      "ID":"map2",
+      "Plugin":["leaflet_draw"]
+      };
+    var squall_map2 = new Map(map_option2);
+    this.ActiveMap.DrawLine = squall_map2;
 
-    }
-
-    //绘制结束时触发的函数
-    function squall_end(){
-      
-    }
+    var map_option3 = {
+      "ID":"map3",
+      "Plugin":["leaflet_draw"]
+      };
+    var squall_map3 = new Map(map_option3);
+    this.ActiveMap.DrawPolygon = squall_map3;
 
     //绘图的参数
 
   },
+  methods:{
+    handleClick(tab, event) {
+      console.log(tab, event);
+      var that = this;
+      setTimeout(function(){
+        console.log(tab.name);
+        that.ActiveMap[that.TabActive].map.invalidateSize();
+      },100)
+    },
+    squall_clear(){
+        this.ActiveMap[this.TabActive].map.invalidateSize();
+    },
+    squall_DrawPoint(e){
+      var squall_Option = {
+        "map":this.ActiveMap[this.TabActive].map,
+        "AimPoint":this.AimPoint
+      }
+
+      if(this.DrawPointLabel=="画点")
+      {
+        this.ActiveMap[this.TabActive].Plugin.DrawPoint(squall_Option);
+        this.DrawPointLabel="结束画点";
+      }  
+      else if(this.DrawPointLabel=="结束画点")
+      {
+        this.ActiveMap[this.TabActive].Plugin.EndDraw(squall_Option);
+        this.DrawPointLabel="画点";
+      }
+    },
+    squall_show(){
+      console.log(this.AimPoint);
+    }
+  }
 }
 </script>
--- Squall_Web_Demo/src/lib/squall_map/leaflet_draw.js
+++ Squall_Web_Demo/src/lib/squall_map/leaflet_draw.js
@@ -1,11 +1,26 @@
 import leaflet_config from "@/lib/squall_map/map_config"
 
-function squall_DrawPoint(Option,Start,End){
-    Start();
+function squall_DrawPoint(Option){
+    if(Option.map)
+    {
+        var squall_Point = Option.AimPoint;
+        Option.map.on("click",function(e){
+            //console.log(e);
+            squall_Point = e.latlng;
+            console.log("在地图上画一个点");
+        })
 
+    }
 
+    return 0;
+}
 
-    End();
+function squall_EndDraw(Option){
+    if(Option.map)
+        Option.map.on("click",function(e){
+            console.log("绘图已经结束了");
+        })
+
     return 0;
 }
 
@@ -36,4 +51,5 @@ export default{
     "DrawLine":squall_DrawLine,
     "DrawPolygon":squall_DrawPolygon,
     "DrawClear":squall_Clear,
+    "EndDraw":squall_EndDraw
 }
