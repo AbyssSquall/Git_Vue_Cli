3c6525b66f8ed321f92c79a9c46f8230a40b7619
Squall <mikeblizzard@live.com>
2019-04-22 17:22:38 +0800

    private

--- Squall_Web_Demo/src/components/Custom.vue
+++ Squall_Web_Demo/src/components/Custom.vue
@@ -6,7 +6,9 @@
       <el-row>
         <div class="squall_Map">
           <div id="map" style="height:100%"></div>
+          <span class="squall_CenterCross el-icon-plus"></span>
         </div>
+        <el-button class="squall_ToolBar" style="margin-top:50px" @click="squall_Warnning()">Warnning!</el-button>
       </el-row>
     </div>
 </template>
@@ -18,7 +20,9 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      page: 'car'
+      page: 'car',
+      Map:{},
+      Sytle:"top:434px",
     }
   },
   components: {
@@ -31,35 +35,48 @@ export default {
     squall_SelectTest(){
       this.basic.squall_basic_http.TestSelect(this);
     },
+    squall_Warnning(){
+      var squall_ResultPoint = this.Map.Plugin.LineSplit(this.Map.getCenter(),[[[29.836983856215246,121.40231098683739],[29.836919890612962,121.41799593432614]],[[29.822016133650935,121.41615118561236],[29.82309423684408,121.4000793122283]]]);
+
+      var squall_graph_option_point = {
+        LayerID:"PolygonExample_point",
+        Type:"point",
+        PointArray:[[[squall_ResultPoint]]],
+        PointStyle:{icon:this.Map.MarkerIconList["primary"]}
+      };
+
+      this.Map.AddGraph(squall_graph_option_point);
+      
+      console.log(squall_ResultPoint);
+    }
   },
   mounted:function(){
     var map_option = {
       "ID":"map",
       "Plugin":["leaflet_custom"]
       };
-    var squall_map = new Map(map_option);
+    this.Map = new Map(map_option);
 
-    squall_map.Plugin.LineSplit(squall_map.getCenter(),[[29.836983856215246,121.40231098683739],[29.836919890612962,121.41799593432614],[29.822016133650935,121.41615118561236],[29.82309423684408,121.4000793122283]]);
-
+    
     var squall_graph_option_line = {
       LayerID:"line1",
       Type:"line",
-      PointArray:[[[[29.836983856215246,121.40231098683739],[29.836919890612962,121.41799593432614],[29.822016133650935,121.41615118561236],[29.82309423684408,121.4000793122283]]]],
+      PointArray:[[[[29.822016133650935,121.41615118561236],[29.82309423684408,121.4000793122283]],[[29.836983856215246,121.40231098683739],[29.836919890612962,121.41799593432614]]]],
       LineStyle:{color: '#1e50a2',weight:2.0,fillOpacity:0.5}
     };
-    squall_map.AddGraph(squall_graph_option_line,function(e){
-        console.log(e);
-      });
-
-    squall_graph_option_line = {
-      LayerID:"line2",
-      Type:"line",
-      PointArray:[[[[29.936983856215246,121.50231098683739],[29.936919890612962,121.51799593432614],[29.922016133650935,121.51615118561236],[29.92309423684408,121.5000793122283]]]],
-      LineStyle:{color: '#ff50a2',weight:2.0,fillOpacity:0.5}
-    };
-    squall_map.AddGraph(squall_graph_option_line,function(e){
+    this.Map.AddGraph(squall_graph_option_line,function(e){
         console.log(e);
     });
+
+    // squall_graph_option_line = {
+    //   LayerID:"line2",
+    //   Type:"line",
+    //   PointArray:[[[[29.836983856215246,121.40231098683739],[29.836919890612962,121.41799593432614]]]],
+    //   LineStyle:{color: '#ff50a2',weight:2.0,fillOpacity:0.5}
+    // };
+    // squall_map.AddGraph(squall_graph_option_line,function(e){
+    //     console.log(e);
+    // });
 
 
     //console.log(squall_map);
--- Squall_Web_Demo/src/lib/squall_map/leaflet_custom.js
+++ Squall_Web_Demo/src/lib/squall_map/leaflet_custom.js
@@ -1,9 +1,43 @@
 function LineSplit(Point,PointArray){
-    console.log(Point);
-    for(var ArrayIndex = 0;ArrayIndex < PointArray.length-1 ;ArrayIndex++)
+    // x = (  B*B*x0  -  A*B*y0  -  A*C  ) / ( A*A + B*B );
+
+    // y  =  ( -A*B*x0 + A*A*y0 - B*C  ) / ( A*A + B*B );
+
+    //d = ( Ax0 + By0 + C ) / sqrt ( A*A + B*B );
+
+    var squall_ReturnPoint = [0,0];
+    var squall_Distance = 100000000;
+
+    //console.log(Point);
+    //console.log(PointArray.length);
+    for(var ArrayIndex = 0;ArrayIndex < PointArray.length;ArrayIndex++)
     {
-        console.log(PointArray[ArrayIndex],PointArray[ArrayIndex+1]);
+        if(PointArray[ArrayIndex][0][0]-PointArray[ArrayIndex][1][0]!=0)
+        {
+            var squall_K = (PointArray[ArrayIndex][0][1]-PointArray[ArrayIndex][1][1])/(PointArray[ArrayIndex][0][0]-PointArray[ArrayIndex][1][0])
+            var squall_B = PointArray[ArrayIndex][0][1] - squall_K*PointArray[ArrayIndex][0][0];
+
+            if(squall_B == PointArray[ArrayIndex][1][1] - squall_K*PointArray[ArrayIndex][1][0])
+                console.log("K=" + squall_K + ",B=" + squall_B);
+            else
+                console.log("wrong answer!",squall_K,squall_B);
+
+            //console.log((-squall_K) * Point.lat);
+            var squall_TempDistance = ( (-squall_K) * Point.lat + Point.lng - squall_B )*( (-squall_K) * Point.lat + Point.lng - squall_B ) / (squall_K * squall_K + 1);
+            console.log(squall_TempDistance);
+            if(squall_Distance>squall_TempDistance)
+            {
+                squall_Distance = squall_TempDistance;
+                squall_ReturnPoint[0] = ( Point.lat + squall_K*Point.lng - squall_K*squall_B ) / ( squall_K*squall_K + 1 );
+                squall_ReturnPoint[1] = ( squall_K*Point.lat + squall_K*squall_K*Point.lng + squall_B ) / (squall_K*squall_K + 1 );
+            }
+        }
+        else
+        {
+            squall_ReturnPoint = [PointArray[ArrayIndex][0][0],Point.lng];
+        } 
     }
+    return squall_ReturnPoint;
 }
 
 function squall_Guide(NowPosition,Route){
