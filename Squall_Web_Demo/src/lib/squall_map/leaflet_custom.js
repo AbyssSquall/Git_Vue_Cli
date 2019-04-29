ae3310a023353bf1c9eb7d49db15e6f8d1d7e93c
Squall <mikeblizzard@live.com>
2019-04-29 16:13:53 +0800

    update

--- Squall_Web_Demo/src/lib/squall_map/leaflet_custom.js
+++ Squall_Web_Demo/src/lib/squall_map/leaflet_custom.js
@@ -1,15 +1,9 @@
 function LineSplit(Point,PointArray){
-    // x = (  B*B*x0  -  A*B*y0  -  A*C  ) / ( A*A + B*B );
-
-    // y  =  ( -A*B*x0 + A*A*y0 - B*C  ) / ( A*A + B*B );
-
-    //d = ( Ax0 + By0 + C ) / sqrt ( A*A + B*B );
 
     var squall_ReturnPoint = [0,0];
+    var squall_ReturnArray = [];
     var squall_Distance = 100000000;
 
-    //console.log(Point);
-    //console.log(PointArray.length);
     for(var ArrayIndex = 0;ArrayIndex < PointArray.length;ArrayIndex++)
     {
         if(PointArray[ArrayIndex][0][0]-PointArray[ArrayIndex][1][0]!=0)
@@ -22,30 +16,34 @@ function LineSplit(Point,PointArray){
             else
                 console.log("wrong answer!",squall_K,squall_B);
 
-            //console.log((-squall_K) * Point.lat);
             var squall_TempDistance = ( (-squall_K) * Point.lat + Point.lng - squall_B )*( (-squall_K) * Point.lat + Point.lng - squall_B ) / (squall_K * squall_K + 1);
-            console.log(squall_TempDistance);
             if(squall_Distance>squall_TempDistance)
             {
                 squall_Distance = squall_TempDistance;
                 squall_ReturnPoint[0] = ( Point.lat + squall_K*Point.lng - squall_K*squall_B ) / ( squall_K*squall_K + 1 );
                 squall_ReturnPoint[1] = ( squall_K*Point.lat + squall_K*squall_K*Point.lng + squall_B ) / (squall_K*squall_K + 1 );
+
+                squall_ReturnArray = [[PointArray[ArrayIndex][0],squall_ReturnPoint],[squall_ReturnPoint,PointArray[ArrayIndex][1]]];
             }
         }
         else
         {
-            squall_ReturnPoint = [PointArray[ArrayIndex][0][0],Point.lng];
+            var squall_TempDistance = PointArray[ArrayIndex][0][0] - Point.lat;
+            if(squall_TempDistance<0)
+                squall_TempDistance = -squall_TempDistance;
+
+            if(squall_Distance>squall_TempDistance)
+            {
+                squall_Distance = squall_TempDistance;
+                squall_ReturnPoint = [PointArray[ArrayIndex][0][0],Point.lng];
+
+                squall_ReturnArray = [[PointArray[ArrayIndex][0],squall_ReturnPoint],[squall_ReturnPoint,PointArray[ArrayIndex][1]]];
+            }
         } 
     }
-    return squall_ReturnPoint;
-}
-
-function squall_Guide(NowPosition,Route){
-    console.log(NowPosition);
-    console.log(Route);
+    return squall_ReturnArray;
 }
 
 export default{
     "LineSplit":LineSplit,
-    "Guide":squall_Guide,
 }
